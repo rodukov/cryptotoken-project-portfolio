c718d785355e8668757a57c6704bebf6a8854f3c
rodukov <electroneuphoria@gmail.com>
2022-06-30 17:19:43 +0300

    update

--- src/components/footers.vue
+++ src/components/footers.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="footer">
-        <p class="default center item" style="margin-left: auto;">NiceToken 2022</p>
+        <p class="default center item" style="margin-left: auto; position: absolute;">NiceToken 2022</p>
         <div class="social">
             <a class="default" href="#"><img alt="Twitter" class="img" src="../assets/icons8-twitter-circled-90.png"></a>
             <a class="default" href="#"><img alt="Github" class="img" src="../assets/icons8-github-90.png"></a>
